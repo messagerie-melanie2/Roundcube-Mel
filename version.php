4617129864747a33c87f799d715c2425468eea1c
Thomas P <thomas.payen@i-carre.net>
2020-05-14 15:31:26 +0200

    New version beta1

--- version.php
+++ version.php
@@ -6,15 +6,13 @@
  * @author PNE Messagerie/Apitech
  */
 class Version {
-
-    /**
-     * Version number
-     */
-    const VERSION = '1.4.8';
-   
-    /**
-     * Build
-     */
-    const BUILD = '20200514142052';
-
+  /**
+   * Version number
+   */
+  const VERSION = '1.4.8b1';
+  
+  /**
+   * Build
+   */
+  const BUILD = '20200514152952';
 }
