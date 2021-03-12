5a80d857e520987ade9da6cc56e4caf8c6ac546d
Thomas P <thomas.payen@i-carre.net>
2021-03-12 12:31:33 +0100

    Update version

--- version.php
+++ version.php
@@ -9,10 +9,10 @@ class Version {
   /**
    * Version number
    */
-  const VERSION = '1.4.8.8';
+  const VERSION = '1.4.9';
   
   /**
    * Build
    */
-  const BUILD = '20210127164052';
+  const BUILD = '20210312123152';
 }
