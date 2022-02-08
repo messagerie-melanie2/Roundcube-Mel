51d9e02aa136c083389bdc0c043b060edcbcf55a
Thomas P <thomas.payen@i-carre.net>
2022-02-08 09:57:37 +0100

    0006485: Permettre de récupérer le numéro de version en cours du webmail

--- version.php
+++ version.php
@@ -16,3 +16,8 @@ class Version {
    */
   const BUILD = '20220107170452';
 }
+
+// Afficher le numéro de version si demandé
+if (isset($_GET['version'])) {
+  echo "Version : " . Version::VERSION . " | Build : " . Version::BUILD;
+}
