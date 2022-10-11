ee1898f868053a7bda50b12a3e445fd5ac977826
Thomas Payen <thomas.payen@i-carre.net>
2022-10-11 09:48:51 +0200

    Mise à jour du build et de la version

--- version.php
+++ version.php
@@ -9,12 +9,12 @@ class Version {
   /**
    * Version number
    */
-  const VERSION = '22.8';
+  const VERSION = '22.9';
   
   /**
    * Build
    */
-  const BUILD = '20220823100452';
+  const BUILD = '20221004095852';
 }
 
 // Afficher le numéro de version si demandé
