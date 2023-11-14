a512e0c00b5afd2db7da24f084d5f738b00745df
Thomas Payen <thomas.payen@i-carre.net>
2023-11-14 16:18:17 +0100

    build(version): 1.4.8.21

--- version.php
+++ version.php
@@ -9,12 +9,12 @@ class Version {
   /**
    * Version number
    */
-  const VERSION = '1.4.8.20';
+  const VERSION = '1.4.8.21';
   
   /**
    * Build
    */
-  const BUILD = '20231114101452';
+  const BUILD = '20231114161752';
 }
 
 // Afficher le numéro de version si demandé
