baa6cfa09d134ca2411843de91ed984275fe21c4
dcotton <dcotton>
2025-12-04 10:44:26 +0100

    feat(signature): :sparkles: possibilité de créer plusieurs signatures et de paramétrer leur utilisation

---
+++ SQL/postgres/2025120300.sql
@@ -0,0 +1,7 @@
+
+
+ALTER TABLE identities
+ADD COLUMN signature_medium text,
+ADD COLUMN html_signature_medium integer DEFAULT 0 NOT NULL,
+ADD COLUMN signature_simple text,
+ADD COLUMN html_signature_simple integer DEFAULT 0 NOT NULL;
