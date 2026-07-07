9f318012b46d957caab231c20252b9a07cf51bf0
mhafdi <mhafdi>
2026-07-07 15:34:07 +0200

    fix(mails): :bug: correction de l'affichage dans la zone de saisie

--- program/js/app.js
+++ program/js/app.js
@@ -10185,7 +10185,7 @@ else xmlhttp.setRequestHeader('X-Roundcube-Request', ref.env.request_token);
 
   this.mailto_handler_uri = function()
   {
-    return location.href.split('?')[0] + '?_task=mail&_action=compose&_to=%s';
+    return location.href.split('?')[0] + '?_task=mail&_action=compose&_to=%s&_extwin=1';
   };
 
   this.register_protocol_handler = function(name)
