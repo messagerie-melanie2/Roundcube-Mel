ed96153f8fdd0688e170831e666fc00a4eeed293
tdelphin <tdelphin>
2026-08-31 15:10:21 +0200

    fix(session): ensure logging only occurs if mel_logs class exists

--- program/lib/Roundcube/rcube_session.php
+++ program/lib/Roundcube/rcube_session.php
@@ -743,7 +743,7 @@ abstract class rcube_session
     function log($line)
     {
         //PAMELA - IP-CHECK LOG
-        if ($this->ip_check) mel_logs::get_instance()->log(mel_logs::WARN, $line);
+        if ($this->ip_check && class_exists('mel_logs')) mel_logs::get_instance()->log(mel_logs::WARN, $line);
         if ($this->logging) {
             rcube::write_log('session', $line);
         }
